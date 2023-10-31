3cc4ac0f5638ae44969acb83eb461bb154fd4c5b
Patradanai Nakpimay <patradanai.na@tnds.com>
2023-11-01 01:24:43 +0700

    feat: job file init

--- cmd/job/main.go
+++ cmd/job/main.go
@@ -1,1 +1,17 @@
 package main
+
+import "flag"
+
+type Args struct {
+	Test string
+}
+
+func main() {
+	var opts Args
+
+	flag.StringVar(&opts.Test, "flag", "default", "flag description")
+
+	flag.Parse()
+
+	// TODO: implement
+}
